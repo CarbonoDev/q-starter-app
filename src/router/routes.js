1917121a59a0f84f285993b9b84ddd8b0ac5c2fa
Cesar Santana <casc.santana@gmail.com>
2018-04-29 20:56:10 -0500

    Separating login route to other group.

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,9 +8,13 @@ Route.view('/', 'layouts/default')
   .children(() => {
     Route.view('', 'index').name('app.home').guard(auth)
     Route.view('info', 'info').name('app.info').guard(auth)
-    Route.view('/login', 'Login').name('app.login').guard(guest)
   })
 
+Route.view('/', 'layouts/default')
+  .guard(web)
+  .children(() => {
+    Route.view('/login', 'Login').name('app.login').guard(guest)
+  })
 Route.view('*', '404').name('app.404')
 
 export default Route.all()
