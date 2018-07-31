a1f7095650811ffa7934747845b6d02f652224f5
Marco Rivadeneyra <marcorivm@gmail.com>
2018-07-31 01:21:05 -0500

    Remove dead code

--- src/services/registration.service.js
+++ src/services/registration.service.js
@@ -4,7 +4,6 @@ import { Config } from 'helpers'
 class Registration extends Service {
   constructor () {
     super()
-    // this.config.origin = Env('BASE_URL')
     this.config.origin = Config('api.api_url')
     this.config.endpoint = '/registrations/'
   }
